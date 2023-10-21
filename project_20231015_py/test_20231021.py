2d24f2096fc0bc477252c614d097e59078b0bf78
wxk38bdc <2227814847@qq.com>
2023-10-22 07:37:41 +0800

    daily python submission 20231022

---
+++ project_20231015_py/test_20231021.py
@@ -0,0 +1,42 @@
+# import random
+
+# # 生成随机数
+# number = random.randint(1, 100)
+
+# # 猜数字
+# while True:
+#     guess = int(input("请猜一个1到100之间的数字："))
+#     if guess == number:
+#         print("恭喜你，猜对了！")
+#         break
+#     elif guess < number:
+#         print("猜的数字太小了，请再试一次。")
+#     else:
+#         print("猜的数字太大了，请再试一次。")
+
+# print("游戏结束。感谢参与！")
+
+# a = 10
+# print(type(a))
+# b=10.12
+# print(type(b))
+# c="hello"
+# print(type(c))
+
+# a=3246546543356513546843513548453354353543313444645644844848200000266569484848494
+# b=666522659683244342784523676634265763467586382765632478658732658376539659326299624759
+# print(a*b)
+
+# a='hello "world"!\'how\' are you?'
+# print(a)
+# print(len(a))
+
+# a=input("请输入一个数字：")
+# b=input("请输入另一个数字：")
+# a=int(a)
+# b=int(b)
+# print(f"两个数字的和是：{a+b}")
+
+# print(2**0.5)
+
+
